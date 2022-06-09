2c82ea3c50ef82925d59e54f72efb4f326cbd7c1
juaniarrigoni <juani.arrigoni@gmail.comgit config --global user.name juaniarrigonigit initgit config --global user.email juani.arrigoni@gmail.com>
2022-06-09 09:56:30 -0300

    HW 03

--- 03-ES6/homework/src/selector.js
+++ 03-ES6/homework/src/selector.js
@@ -1,4 +1,4 @@
-var traverseDomAndCollectElements = function(matchFunc, startEl) {
+var traverseDomAndCollectElements = function (matchFunc, startEl) {
   var resultSet = [];
 
   if (typeof startEl === "undefined") {
@@ -9,16 +9,27 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
   // usa matchFunc para identificar elementos que matchien
 
   // TU CÓDIGO AQUÍ
-  
+  if (matchFunc(startEl)) {
+    resultSet.push(startEl);
+  }
+
+  for (let i = 0; i < startEl.children.length; i++) {
+    let child = starEl.children[i];
+    let collectedEl = traverseDomAndCollectElements(matchFunc, child);
+    resultSer = [...resultSet, ...collectedEl];
+  }
+  return resultSet;
 };
 
 // Detecta y devuelve el tipo de selector
 // devuelve uno de estos tipos: id, class, tag.class, tag
 
-
-var selectorTypeMatcher = function(selector) {
+var selectorTypeMatcher = function (selector) {
   // tu código aquí
-  
+  if (selector[0] === "#") return "id";
+  if (selector[0] === ".") return "class";
+  if ((selector.split("."), length)) return "tag.class";
+  return "tag";
 };
 
 // NOTA SOBRE LA FUNCIÓN MATCH
@@ -26,22 +37,35 @@ var selectorTypeMatcher = function(selector) {
 // parametro y devuelve true/false dependiendo si el elemento
 // matchea el selector.
 
-var matchFunctionMaker = function(selector) {
+var matchFunctionMaker = function (selector) {
   var selectorType = selectorTypeMatcher(selector);
   var matchFunction;
-  if (selectorType === "id") { 
-   
+  if (selectorType === "id") {
+    matchFunction = (element) => "#" + element.id === selector;
   } else if (selectorType === "class") {
-    
+    matchFunction = (element) => {
+      let clases = element.classList;
+      for (let i = 0; i < clases.length; i++) {
+        if ("." + clases[i] === selector) {
+          return true;
+        }
+      }
+      return false;
+    };
   } else if (selectorType === "tag.class") {
-    
+    matchFunction = (element) => {
+      let [t, c] = selector.split(".");
+      return (
+        matchFunctionMaker(t)(element) && matchFunctionMaker("." + c)(element)
+      );
+    };
   } else if (selectorType === "tag") {
-    
+    matchFunction = (element) => element.tagName == selector.toUpperaCase();
   }
   return matchFunction;
 };
 
-var $ = function(selector) {
+var $ = function (selector) {
   var elements;
   var selectorMatchFunc = matchFunctionMaker(selector);
   elements = traverseDomAndCollectElements(selectorMatchFunc);
